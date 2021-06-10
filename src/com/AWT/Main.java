66ca49b788cd395f30c6cc4fe25a1dadb837c37e
timofeya123 <timofeya123@inbox.ru>
2021-06-10 21:19:08 +0600

    comment

--- src/com/AWT/Main.java
+++ src/com/AWT/Main.java
@@ -6,42 +6,48 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public class Main {
+public class Main { // property - как map только через файл
 
     static void SaveProperties() throws IOException {
-        Properties property = new Properties();
-        property.setProperty("db.host","http://localhost:8888/mydb");
+        Properties property = new Properties(); // наш пропертис
+        property.setProperty("db.host","http://localhost:8888/mydb"); // сохраняем его значения
         property.setProperty("db.login","root");
         property.setProperty("db.password","dbroot");
+
+        // что мы сохраняем, объяснение
         System.out.println("Save: "+property.getProperty("db.host")+" "+property.getProperty("db.login")+" "+property.getProperty("db.password"));
+
+        // сохранение в файл
         FileOutputStream out = new FileOutputStream("config.property");
         property.store(out, "my comment");
     }
 
     public static void main(String[] args) throws IOException {
-        SaveProperties();
+        SaveProperties(); // наше сохранение
         System.out.println("-----");
-        LoadProperties();
+        LoadProperties(); // наша загрузка
     }
 
     static void LoadProperties(){
+        // поток из файла
         FileInputStream fis;
-        Properties property = new Properties();
+        Properties property = new Properties(); // новый пропертис
 
         try {
-            fis = new FileInputStream("config.property");
-            property.load(fis);
+            fis = new FileInputStream("config.property"); // файл с данными
+            property.load(fis);// оправляем его значения в пропертис
 
-            String host = property.getProperty("db.host");
+            String host = property.getProperty("db.host"); // выгружаем значения в переменные
             String login = property.getProperty("db.login");
             String password = property.getProperty("db.password");
 
+            // проверяем, что всё работает
             System.out.println("HOST: " + host
                     + ", LOGIN: " + login
                     + ", PASSWORD: " + password);
 
         } catch (IOException e) {
-            System.err.println("ОШИБКА: Файл свойств отсуствует!");
+            System.err.println("ОШИБКА: Файл свойств отсуствует!"); // ошибка
         }
     }
 }
